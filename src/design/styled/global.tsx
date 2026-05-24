7de7f5e7e45b5205c6d591aa7b0b0348c6f64370
Bally.Lomibao <bally.lomibao@howickltd.com>
2026-05-25 01:26:46 +1000

    [fix]: update StandardAppBar background color to contrast black

--- src/design/styled/global.tsx
+++ src/design/styled/global.tsx
@@ -2,5 +2,5 @@ import { AppBar, styled } from '@mui/material'
 import { designTokens } from 'design/token'
 
 export const StandardAppBar = styled(AppBar)(() => ({
-  background: designTokens.color.neutral.ink
+  background: designTokens.color.contrast.black
 }))
