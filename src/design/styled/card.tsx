2ee60aba3a47db5d08aeb4263f68c64fba9261bf
Bally.Lomibao <bally.lomibao@howickltd.com>
2026-05-25 01:26:27 +1000

    [formatting]

--- src/design/styled/card.tsx
+++ src/design/styled/card.tsx
@@ -5,7 +5,7 @@ import { designTokens } from 'design/token'
 export const FillCard = styled(Card)(() => ({
     display      : 'flex',
     flexDirection: 'column',
-    height       : '100%',
+    height       : '100%'
 }))
 
 export const FillCardContent = styled(CardContent)(({ theme }) => ({
