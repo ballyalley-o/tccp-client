8efffeede23658b6e1747fba4388a3598f99167e
Bally.Lomibao <bally.lomibao@howickltd.com>
2026-05-26 18:25:09 +1000

    [config]: added career option keys

--- src/config/key.config.ts
+++ src/config/key.config.ts
@@ -1,3 +1,18 @@
 export const KEY = {
-    ROLE: ['student', 'trainer', 'admin'] as const
+    ROLE: ['student', 'trainer', 'admin'] as const,
+    CAREER_OPTION: [
+        'Web Development',
+        'Mobile Development',
+        'UI/UX',
+        'Data Science',
+        'Data Security',
+        'Software Engineering',
+        'Full Stack Web Development',
+        'Dev Ops',
+        'AI',
+        'Machine Learning',
+        'Cloud Computing',
+        'Cyber Security',
+        'Other'
+    ] as const
 }
