882f03c4dbecee388db101326eed1bb10f5ae2aa
Bally.Lomibao <bally.lomibao@howickltd.com>
2026-05-26 18:09:29 +1000

    [config]

--- src/config/nav.config.ts
+++ src/config/nav.config.ts
@@ -1,30 +1,68 @@
-import { urlBuilder } from "lib/tool"
+import { urlBuilder, type LocaleKey } from "lib/tool"
 
-export const NAV = {
+type NavItemType = {
+    id   : string
+    label: LocaleKey
+    value: string
+    href : string
+}
+
+type NavType = {
+    USER  : NavItemType[],
+    FOOTER: NavItemType[]
+}
+
+export const NAV: NavType = {
+    USER  : [
+        {
+            id   : 'account',
+            label: 'nav.user.account',
+            value: 'account',
+            href: urlBuilder('account')
+        },
+        {
+            id   : 'manage',
+            label: 'nav.user.manage',
+            value: 'manage',
+            href: urlBuilder('manage')
+        },
+        {
+            id   : 'setting',
+            label: 'nav.user.setting',
+            value: 'setting',
+            href: urlBuilder('setting')
+        },
+        {
+            id   : 'log-out',
+            label: 'nav.user.log_out',
+            value: 'log-out',
+            href: urlBuilder('log-out')
+        }
+    ],
     FOOTER: [
         {
             id   : 'docs-1',
-            label: 'Docs',
+            label: 'nav.footer.docs',
             value: 'docs',
-            path : urlBuilder('docs')
+            href : urlBuilder('docs')
         },
         {
             id   : 'api-2',
-            label: 'API',
+            label: 'nav.footer.api',
             value: 'api',
-            path:  urlBuilder('api')
+            href:  urlBuilder('api')
         },
         {
             id   : 'career-3',
-            label: 'Careers',
+            label: 'nav.footer.careers',
             value: 'career',
-            path : urlBuilder('career')
+            href : urlBuilder('career')
         },
         {
             id   : 'privacy-4',
-            label: 'Privacy',
+            label: 'nav.footer.privacy',
             value: 'privacy',
-            path:  urlBuilder('career')
+            href:  urlBuilder('career')
         },
     ]
 }
