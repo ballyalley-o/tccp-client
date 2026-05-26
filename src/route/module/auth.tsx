48a175049ef6e7e096fae39eaaf53a85e2b86c77
Bally.Lomibao <bally.lomibao@howickltd.com>
2026-05-26 18:25:59 +1000

    [route]: route for auth pages

--- src/route/module/auth.tsx
+++ src/route/module/auth.tsx
@@ -1,7 +1,8 @@
 import type { RouteObject } from 'react-router-dom'
 import { AuthGuard } from 'route/guard'
 import { PATH } from 'route/path'
-import { AuthLogInPage, AuthRegisterPage } from 'route/element'
+import { AppProtectedRoute } from 'component/app/app-protected-route'
+import { AuthAccountPage, AuthLogInPage, AuthRegisterPage, AuthSettingPage, ManagePage } from 'route/element'
 
 const authRoute: RouteObject[] = [
     {
@@ -15,6 +16,28 @@ const authRoute: RouteObject[] = [
                     path   : PATH.AUTH.REGISTER,
                     element: <AuthRegisterPage />
                 }
+        ]
+    },
+    {
+        element: <AppProtectedRoute />,
+        children: [
+            {
+                path   : PATH.AUTH.ACCOUNT,
+                element: <AuthAccountPage />
+            },
+            {
+                path   : PATH.AUTH.SETTING,
+                element: <AuthSettingPage />
+            }
+        ]
+    },
+    {
+        element: <AppProtectedRoute roles={['trainer', 'admin']} />,
+        children: [
+            {
+                path   : PATH.AUTH.MANAGE,
+                element: <ManagePage />
+            }
         ]
     }
 ]
