49038ada23f6d16b80ac53cee380ca823616a1c5
Bally.Lomibao <bally.lomibao@howickltd.com>
2026-05-26 14:21:18 +1000

    [design]: added new typography variants

--- src/design/styled/typography.tsx
+++ src/design/styled/typography.tsx
@@ -1,5 +1,5 @@
-import { Typography } from "@mui/material"
-import { styled } from '@mui/material/styles'
+import { Typography }   from "@mui/material"
+import { styled }       from '@mui/material/styles'
 import { designTokens } from "design/token"
 
 export const FormTitle = styled(Typography)(() => ({
@@ -22,3 +22,20 @@ export const IntroText = styled(Typography)(({ theme }) => ({
 export const SmallOffsetText = styled(Typography)(({ theme }) => ({
     marginTop: theme.spacing(designTokens.spacing.sm),
 }))
+
+export const FooterWrapperText = styled(Typography)({
+    maxWidth: 320
+})
+
+export const FooterText = styled(Typography)({
+    cursor    : 'pointer',
+    transition: '0.2s ease',
+    '&:hover' : {
+        color: 'text.primary'
+    }
+})
+
+export const FooterMonoText = styled(Typography)({
+    fontFamily   : 'monospace',
+    letterSpacing: '0.5'
+})
