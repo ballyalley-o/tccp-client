9db69521eb3d332768df45c5b9261d05b3516987
Bally.Lomibao <bally.lomibao@howickltd.com>
2026-05-27 18:51:07 +1000

    [feat]: added ne styled components

--- src/design/styled/box.tsx
+++ src/design/styled/box.tsx
@@ -12,7 +12,7 @@ export const AppRootBox = styled(Box)(({ theme }) => ({
 
 export const AuthPanel = styled(Box, { shouldForwardProp: (prop) => prop !== 'wide' })<{ wide?: boolean }>(({ wide }) => ({
     marginInline: 'auto',
-    maxWidth    : wide ? designTokens.layout.formWideMaxWidth: designTokens.layout.formMaxWidth,
+    minWidth    : wide ? designTokens.layout.formWideMaxWidth: designTokens.layout.formMaxWidth,
 }))
 
 export const IconTile = styled(Box)(({ theme }) => ({
@@ -52,3 +52,78 @@ export const FooterBox = styled(Box)(({ theme }) => ({
     backgroundColor: theme.palette.success.main,
     boxShadow      : `0 0 12px ${theme.palette.success.main}`
 }))
+
+export const AuthBgBox = styled(Box)({
+    minHeight      : designTokens.layout.appMinHeight,
+    position       : 'relative',
+    overflow       : 'hidden',
+    backgroundColor: designTokens.color.neutral.ink,
+    backgroundImage: designTokens.color.gradient.auth_background,
+    color          : designTokens.color.dark_mode.white,
+})
+
+export const AuthBgLogoBox = styled(Box)(({ theme }) => ({
+  position                    : 'absolute',
+  top                         : 24,
+  left                        : 24,
+  zIndex                      : 2,
+  [theme.breakpoints.up('md')]: {
+    top: 32,
+    left: 40
+  },
+  '& img' : {
+    height: 44,
+    filter: 'brightness(0) invert(1)',
+  },
+}))
+
+export const AuthMainBox = styled(Box)<{ component?: ElementType }>(({ theme }) => ({
+  alignItems       : 'center',
+  display          : 'flex',
+  justifyContent   : 'center',
+  minHeight        : designTokens.layout.appMinHeight,
+  paddingRight     : 2,
+  paddingLeft      : 2,
+  paddingTop       : 12,
+  paddingBottom    : 12,
+  '& .MuiCard-root': {
+    backgroundColor: 'rgba(24, 31, 41, 0.94)',
+    border         : '1px solid rgba(148, 163, 184, 0.28)',
+    boxShadow      : '0 24px 72px rgba(0, 0, 0, 0.36)',
+    color          : designTokens.color.dark_mode.white,
+  },
+  '& .MuiInputLabel-root': {
+    color: 'rgba(244, 247, 251, 0.78)',
+  },
+  '& .MuiOutlinedInput-root': {
+    color          : '#F4F7FB',
+    backgroundColor: 'rgba(9, 13, 20, 0.28)',
+  },
+  '& .MuiOutlinedInput-notchedOutline': {
+    borderColor: 'rgba(203, 213, 225, 0.36)',
+  },
+  '& .MuiOutlinedInput-root:hover .MuiOutlinedInput-notchedOutline': {
+    borderColor: alpha(designTokens.color.brand.primary, 0.72),
+  },
+  '& .MuiInputBase-input::placeholder': {
+    color  : 'rgba(244, 247, 251, 0.58)',
+    opacity: 1,
+  },
+  '& .MuiDivider-root': {
+    borderColor: 'rgba(203, 213, 225, 0.34)',
+  },
+  [theme.breakpoints.up('sm')]: {
+    paddingRight: 3,
+    paddingLeft : 3,
+  },
+  [theme.breakpoints.up('md')]: {
+    paddingTop   : 12,
+    paddingBottom: 12,
+  },
+}))
+
+export const LogoBox = styled(Box)({
+  '& img' : {
+    filter: 'brightness(0) invert(0)',
+  }
+})
