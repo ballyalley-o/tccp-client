911700d453c38b05014f03c4dd470aa2fa8f8bc6
Bally.Lomibao <bally.lomibao@howickltd.com>
2026-05-27 23:35:24 +1000

    [feat]: add additional font styles and update asset directory

--- src/component/app/index.ts
+++ src/component/app/index.ts
@@ -1,3 +1,4 @@
 export { default as AppLogo } from './app-logo'
+export * from './app-logo'
 export { default as AppFooter } from './app-footer'
 export { default as AppAccountMenu } from './app-account-menu'
--- src/config/dir/asset.dir.ts
+++ src/config/dir/asset.dir.ts
@@ -3,5 +3,6 @@ import { pathBuilder } from "lib/tool"
 const _ASSET = 'asset'
 
 export const ASSET_DIR = {
-    LOGO_PNG: pathBuilder(_ASSET, 'logo.png')
+    LOGO_PNG      : pathBuilder(_ASSET, 'logo.png'),
+    LOGO_DESAT_PNG: pathBuilder(_ASSET, 'logo-desat.png'),
 }
