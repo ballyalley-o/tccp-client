d81783f10c37c77a00361a2818685a675b05039a
Bally.Lomibao <bally.lomibao@howickltd.com>
2026-05-27 23:07:59 +1000

    [style]: update names

--- src/design/token/color.ts
+++ src/design/token/color.ts
@@ -1,26 +1,36 @@
 export const color = {
   brand: {
-    primary    : '#5F6368',
-    primaryDark: '#174EA6',
-    secondary  : '#0B806A',
-    accent     : '#F9E400',
+    primary     : '#5F6368',
+    primary_dark: '#32363a',
+    secondary   : '#3148b0',
+    accent      : '#F9E400',
   },
   status: {
     error  : '#EE1F14',
     success: '#B3FF00',
   },
   neutral: {
-    ink       : '#12161C',
-    muted     : '#5F6368',
-    surface   : '#EEf2f9',
-    surfaceAlt: '#F7F9FC',
-    border    : '#FFFFFF',
+    ink        : '#12161C',
+    muted      : '#5F6368',
+    surface    : '#EEf2f9',
+    surface_alt: '#F7F9FC',
+    border     : '#FFFFFF',
   },
   contrast: {
     white: '#FFFFFF',
     black: '#030303',
   },
-  darkMode: {
-    white: '#F2EED8'
+  dark_mode: {
+    white: '#F2EED8',
+  },
+  action: {
+    hover_primary_dark: '#1E242F',
+  },
+  gradient: {
+    auth_background: `
+      radial-gradient(circle at 86% 8%, rgba(22, 74, 147, 0.55), transparent 24%),
+      radial-gradient(circle at 70% 42%, rgba(14, 105, 151, 0.34), transparent 26%),
+      linear-gradient(110deg, #050204 0%, #030713 44%, #051A3B 70%, #062B3C 100%)
+    `,
   },
 }
