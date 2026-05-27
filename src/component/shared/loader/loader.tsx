ef601cb09f8540f54eedc184c93d5562273ca438
Bally.Lomibao <bally.lomibao@howickltd.com>
2026-05-27 13:30:08 +1000

    [fix]: loader alignment fixed

--- src/component/shared/loader/loader.tsx
+++ src/component/shared/loader/loader.tsx
@@ -1,14 +1,13 @@
-import { CircularProgress, Stack, Typography } from '@mui/material'
-import { PageCentered } from 'design/styled'
+import { CircularProgress, Stack } from '@mui/material'
+import { PageCentered, MonoText } from 'design/styled'
 
 const Loader = () => {
+  const _TEXT = '[ loading ]'
   return (
-    <PageCentered maxWidth="sm">
-      <Stack alignItems="center" spacing={2}>
-        <CircularProgress aria-label="Loading page" />
-        <Typography color="text.secondary" variant="body2">
-          Loading...
-        </Typography>
+    <PageCentered maxWidth={'lg'}>
+      <Stack alignItems={'center'} spacing={2}>
+        <CircularProgress aria-label={'loading_circular'} />
+        <MonoText color={'text.secondary'} variant={'body2'}>{_TEXT}</MonoText>
       </Stack>
     </PageCentered>
   )
