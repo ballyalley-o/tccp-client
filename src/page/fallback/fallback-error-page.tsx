f4c922e4e5ccf8d9a2e7a58805835f1bc3ba2828
Bally.Lomibao <bally.lomibao@howickltd.com>
2026-05-27 22:37:27 +1000

    [feat]: enhance FallbackErrorPage layout and styling with new components

--- src/page/fallback/fallback-error-page.tsx
+++ src/page/fallback/fallback-error-page.tsx
@@ -1,37 +1,43 @@
 import { useRouteError, isRouteErrorResponse } from 'react-router-dom'
-import { Typography, Alert, Button } from '@mui/material'
+import { Typography, Alert, Button, Stack } from '@mui/material'
 import ErrorOutlineIcon from '@mui/icons-material/ErrorOutline'
-import { AlertBlock, PageCentered } from '../../design/styled'
+import { AppLogo } from 'component/app'
+import { AlertBlock, FallbackBgContainer, MonoText, LogoBox } from 'design/styled'
 import { transl } from 'lib/tool'
+import { GLOBAL } from 'config/global.config'
 
 const FallbackErrorPage = () => {
-  const error = useRouteError()
-
-  const message = isRouteErrorResponse(error)
+  const error    = useRouteError()
+  const dateTime = new Date().toISOString()
+  const message  = isRouteErrorResponse(error)
     ? error.statusText || error.data?.message
     : error instanceof Error
       ? error.message
       : transl('error.generic')
 
   return (
-    <PageCentered maxWidth={'sm'}>
-      <AlertBlock>
-        <Alert
-          action={
-            <Button color='inherit' onClick={() => window.location.reload()} size='small'>
-              Retry
-            </Button>
-          }
-          icon={<ErrorOutlineIcon />}
-          severity='error'
-        >
-          <Typography component='h1' fontWeight={700} gutterBottom variant='subtitle1'>
-            {transl('error.unable_load_page')}
-          </Typography>
-          <Typography variant='body2'>{message}</Typography>
-        </Alert>
-      </AlertBlock>
-    </PageCentered>
+    <FallbackBgContainer maxWidth={'xl'}>
+      <Stack spacing={4} width={'100%'} alignItems={'center'}>
+        <LogoBox flexDirection={'column'}>
+          <AppLogo/>
+        </LogoBox>
+        <AlertBlock sx={{ width: '100%', maxWidth: 520 }}>
+          <Alert
+            action={<Button color='inherit' onClick={() => window.location.reload()} size={'small'}>{transl('retry')}</Button>}
+            icon={<ErrorOutlineIcon />}
+            severity={'error'}>
+            <Typography component='h1' fontWeight={700} gutterBottom variant='subtitle1'>
+              {transl('error.unable_load_page')}
+            </Typography>
+            <Typography variant={'body2'}>{message}</Typography>
+          </Alert>
+          <Stack width={'100%'}>
+            <MonoText variant={'caption'} color={'primary'}>{GLOBAL.APP_VERSION}</MonoText>
+            <MonoText variant={'caption'} color={'primary'}>{dateTime}</MonoText>
+          </Stack>
+        </AlertBlock>
+      </Stack>
+    </FallbackBgContainer>
   )
 }
 
