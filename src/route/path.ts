be746456a81b6d3d47952c81525fe02df2b1733e
Bally.Lomibao <bally.lomibao@howickltd.com>
2026-05-29 13:13:43 +1000

    [feat]: added admin paths

--- src/route/path.ts
+++ src/route/path.ts
@@ -1,15 +1,22 @@
-import { pathBuilder } from "lib/tool";
+import { pathBuilder } from 'lib/tool'
 
 const _DASHBOARD = 'dashboard'
 const _AUTH      = 'auth'
 const _BOOTCAMP  = 'bootcamp'
 const _COURSE    = 'course'
 
+const _ADMIN = 'admin'
+
 const _BOOTCAMP_SLUG = ':bootcampSlug'
 
 export const PATH = {
   ROOT: '/',
   DASHBOARD: pathBuilder(_DASHBOARD),
+  ADMIN: {
+    ROOT    : pathBuilder(_ADMIN),
+    USER    : pathBuilder(_ADMIN, 'user'),
+    BOOTCAMP: pathBuilder(_ADMIN, 'bootcamp')
+  },
   AUTH: {
     ROOT    : pathBuilder(_AUTH),
     LOG_IN  : pathBuilder(_AUTH, 'log-in'),
