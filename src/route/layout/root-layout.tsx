8f50ed01ff0350035eeaba8a1891317a7f12625a
Bally.Lomibao <bally.lomibao@howickltd.com>
2026-05-29 10:58:19 +1000

    [refactor]

--- src/route/layout/root-layout.tsx
+++ src/route/layout/root-layout.tsx
@@ -1,36 +1,14 @@
-import { Link as RouterLink, Outlet } from 'react-router-dom'
+import { Outlet } from 'react-router-dom'
 import { useAppSelector } from 'app/hook'
-import { PATH } from 'route/path'
-import { Button, Stack, Toolbar, Typography } from '@mui/material'
-import MenuBookIcon from '@mui/icons-material/MenuBook'
-import { AppLogo, AppFooter, AppAccountMenu } from 'component/app'
-import { AppContent, AppRootBox, StandardAppBar } from 'design/styled'
-import { formatText, transl } from 'lib/tool'
+import { AppFooter, AppRootAppbar } from 'component/app'
+import { AppContent, AppRootBox } from 'design/styled'
 
 const RootLayout = () => {
   const { user } = useAppSelector((state) => state.auth)
 
   return (
     <AppRootBox>
-      <StandardAppBar position={'sticky'} elevation={0}>
-        <Toolbar>
-          <AppLogo />
-
-          <Stack direction='row' spacing={1} alignItems='center'>
-            <Button component={RouterLink} to={PATH.BOOTCAMP.ROOT} startIcon={<MenuBookIcon />} color='inherit'>
-              {transl('bootcamps')}
-            </Button>
-            {user ? (
-              <AppAccountMenu user={user} />
-            ) : (
-              <Button component={RouterLink} to={PATH.AUTH.LOG_IN} variant={'contained'} color={'warning'}>
-                <Typography variant={'body1'}>{formatText(transl('log_in'), 'uppercase')}</Typography>
-              </Button>
-            )}
-          </Stack>
-        </Toolbar>
-      </StandardAppBar>
-
+      <AppRootAppbar user={user} />
       <AppContent maxWidth={false}>
         <Outlet />
       </AppContent>
