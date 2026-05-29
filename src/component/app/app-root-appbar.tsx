6c6a82798a5cad125966b8eb1d6da3220a6a9928
Bally.Lomibao <bally.lomibao@howickltd.com>
2026-05-29 10:55:30 +1000

    [enhance]

--- src/component/app/app-root-appbar.tsx
+++ src/component/app/app-root-appbar.tsx
@@ -2,11 +2,13 @@
 import { Link as RouterLink } from 'react-router-dom'
 import { PATH } from 'route/path'
 import type { User } from 'types'
-import { Toolbar, Stack, Button, Typography } from '@mui/material'
+import { Toolbar, Stack, Button } from '@mui/material'
+import DashboardIcon from '@mui/icons-material/Dashboard'
 import MenuBookIcon from '@mui/icons-material/MenuBook'
+import LoginSharpIcon from '@mui/icons-material/LoginSharp'
 import { AppLogo, AppAccountMenu } from 'component/app'
 import { StandardAppbar } from 'design/styled'
-import { formatText, transl } from 'lib/tool'
+import { transl } from 'lib/tool'
 
 const AppRootAppbar = ({ user }: { user: User | null }) => {
   return (
@@ -15,14 +17,17 @@ const AppRootAppbar = ({ user }: { user: User | null }) => {
         <AppLogo />
 
         <Stack direction='row' spacing={1} alignItems='center'>
+            <Button component={RouterLink} to={PATH.DASHBOARD} startIcon={<DashboardIcon />} color='inherit'>
+            {transl('nav.dashboard')}
+            </Button>
             <Button component={RouterLink} to={PATH.BOOTCAMP.ROOT} startIcon={<MenuBookIcon />} color='inherit'>
             {transl('bootcamps')}
             </Button>
             {user ? (
             <AppAccountMenu user={user} />
             ) : (
-            <Button component={RouterLink} to={PATH.AUTH.LOG_IN} variant={'contained'} color={'warning'}>
-                <Typography variant={'body1'}>{formatText(transl('log_in'), 'uppercase')}</Typography>
+            <Button component={RouterLink} to={PATH.AUTH.LOG_IN} variant={'outlined'} color={'primary'}>
+                <LoginSharpIcon />
             </Button>
             )}
         </Stack>
