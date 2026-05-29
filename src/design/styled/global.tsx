984f68bbcb113974b602f0e35dd55ab5813744dd
Bally.Lomibao <bally.lomibao@howickltd.com>
2026-05-29 10:56:00 +1000

    [enhance]

--- src/design/styled/global.tsx
+++ src/design/styled/global.tsx
@@ -1,6 +1,6 @@
 import { AppBar, styled } from '@mui/material'
 import { designTokens } from 'design/token'
 
-export const StandardAppBar = styled(AppBar)(() => ({
+export const StandardAppbar = styled(AppBar)(() => ({
   background: designTokens.color.contrast.black
 }))
