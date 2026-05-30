6092081be4f5ae7e78ddefba61d0ba3410e23f85
Bally.Lomibao <bally.lomibao@howickltd.com>
2026-05-30 17:52:31 +1000

    [refactor]: adjust indentation and formatting for route exports in element.tsx

--- src/route/element.tsx
+++ src/route/element.tsx
@@ -3,10 +3,11 @@ import { Loadable } from "route/loadable"
 
 export const DashboardPage      = Loadable(lazy(() => import("page/dashboard/dashboard-page")))
 export const BootcampListPage   = Loadable(lazy(() => import('page/bootcamp/bootcamp-list-page')))
-export const BootcampDetailPage = Loadable(lazy(() => import('page/bootcamp/bootcamp-detail-page')))
+    export const BootcampDetailPage = Loadable(lazy(() => import('page/bootcamp/bootcamp-detail-page')))
+
 // auth
-export const AuthLogInPage    = Loadable(lazy(() => import('page/auth/auth-login-page')))
-export const AuthRegisterPage = Loadable(lazy(() => import('page/auth/auth-register-page')))
-export const AuthAccountPage  = Loadable(lazy(() => import('page/auth/auth-account-page')))
-export const AuthSettingPage  = Loadable(lazy(() => import('page/auth/auth-account-update-page')))
-export const ManagePage       = Loadable(lazy(() => import('page/manage-page')))
+export const AuthLogInPage         = Loadable(lazy(() => import('page/auth/auth-login-page')))
+export const AuthRegisterPage      = Loadable(lazy(() => import('page/auth/auth-register-page')))
+export const AuthAccountPage       = Loadable(lazy(() => import('page/auth/auth-account-page')))
+    export const AuthAccountUpdatePage = Loadable(lazy(() => import('page/auth/auth-account-update-page')))
+export const ManagePage            = Loadable(lazy(() => import('page/manage-page')))
