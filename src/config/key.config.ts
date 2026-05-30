c45843c1ed5f49e146dc840c70d0b5c059ce1ace
Bally.Lomibao <bally.lomibao@howickltd.com>
2026-05-31 02:36:15 +1000

    [config]: added new keys

--- src/config/key.config.ts
+++ src/config/key.config.ts
@@ -1,18 +1,28 @@
+import type { Bootcamp } from 'types'
+
 export const KEY = {
-    ROLE: ['student', 'trainer', 'admin'] as const,
+    ROLE         : ['student', 'trainer', 'admin'] as const,
     CAREER_OPTION: [
-        'Web Development',
-        'Mobile Development',
-        'UI/UX',
-        'Data Science',
-        'Data Security',
-        'Software Engineering',
-        'Full Stack Web Development',
-        'Dev Ops',
-        'AI',
-        'Machine Learning',
-        'Cloud Computing',
-        'Cyber Security',
-        'Other'
-    ] as const
+        'career_options.web_development',
+        'career_options.mobile_development',
+        'career_options.ui_ux',
+        'career_options.data_science',
+        'career_options.data_security',
+        'career_options.software_engineering',
+        'career_options.full_stack_web_development',
+        'career_options.dev_ops',
+        'career_options.ai',
+        'career_options.machine_learning',
+        'career_options.cloud_computing',
+        'career_options.cyber_security',
+        'career_options.other'
+    ] as const,
+    MINIMUM_SKILL: ['beginner', 'intermediate', 'advanced'] as const,
+    TRAIT        : ['housing', 'jobAssistance', 'jobGuarantee', 'acceptGi'] as const,
+    TRAIT_OPTION : (selected: Bootcamp) => [
+        ['traits.housing', selected.housing],
+        ['traits.job_assistance', selected.jobAssistance],
+        ['traits.job_guarantee', selected.jobGuarantee],
+        ['traits.gi_bill_accepted', selected.acceptGi],
+    ]
 }
