7378bece49c8c5625bc38c796e90f323ecc6a03a
Bally.Lomibao <bally.lomibao@howickltd.com>
2026-05-30 15:37:50 +1000

    [feat]: new global component added

--- src/design/styled/global.tsx
+++ src/design/styled/global.tsx
@@ -2,5 +2,5 @@ import { AppBar, styled } from '@mui/material'
 import { designTokens } from 'design/token'
 
 export const StandardAppbar = styled(AppBar)(() => ({
-  background: designTokens.color.contrast.black
+  background: designTokens.color.contrast.black,
 }))
