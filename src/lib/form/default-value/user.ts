47a9d034ab66c1d33936d80fbae3c6b14158c7b5
Bally.Lomibao <bally.lomibao@howickltd.com>
2026-05-30 15:47:19 +1000

    [feat]: update user default values for login and registration

--- src/lib/form/default-value/user.ts
+++ src/lib/form/default-value/user.ts
@@ -1,12 +1,28 @@
-import type { User } from 'types'
+import type { User, UserRole } from 'types'
 
-export const USER = (user?: User) => {
+export const loginUserDefaultValue = {
+    email   : '',
+    password: '',
+}
+export const registerUserDefaultValue =  (role: UserRole) => {
+   return {
+        firstname   : '',
+        lastname    : '',
+        username    : '',
+        email       : '',
+        password    : '',
+        role,
+        organization: '',
+   }
+}
+
+export const updateUserDefaultValue = (user?: User) => {
     return {
-      firstname: user?.firstname || '',
-      lastname : user?.lastname || '',
-      username : user?.username || '',
-      email    : user?.email || '',
-      location : user?.location || '',
-      avatar   : user?.avatar || '',
+        firstname: user?.firstname || '',
+        lastname : user?.lastname || '',
+        username : user?.username || '',
+        email    : user?.email || '',
+        location : user?.location || '',
+        avatar   : user?.avatar || '',
     }
 }
