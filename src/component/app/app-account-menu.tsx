e7ea1c15b7654d781723cf8afd29602fd1282a90
Bally.Lomibao <bally.lomibao@howickltd.com>
2026-05-30 16:35:53 +1000

    [refactor]: streamline account menu by utilizing NAV configuration and removing unused imports

--- src/component/app/app-account-menu.tsx
+++ src/component/app/app-account-menu.tsx
@@ -1,15 +1,13 @@
 import { Fragment, useState, type MouseEvent } from 'react'
 import { Link as RouterLink, useNavigate } from 'react-router-dom'
-import AccountCircleIcon from '@mui/icons-material/AccountCircle'
-import LogoutSharpIcon from '@mui/icons-material/LogoutSharp'
-import SettingsIcon from '@mui/icons-material/Settings'
-import ShieldIcon from '@mui/icons-material/Shield'
-import { Avatar, Divider, IconButton, ListItemIcon, Menu, MenuItem, Stack, Tooltip, Typography } from '@mui/material'
-import { useAppDispatch } from 'app/hook'
-import { logout } from 'app/store/slice/auth/auth-slice'
-import { PATH } from 'route/path'
 import type { User } from 'types/model'
+import { logout } from 'app/store/slice/auth/auth-slice'
+import { useAppDispatch } from 'app/hook'
+import { PATH } from 'route/path'
+import { Divider, IconButton, ListItemIcon, Menu, MenuItem, Stack, Tooltip, Typography } from '@mui/material'
+import { AppUserAvatar } from 'component/app'
 import { transl } from 'lib/tool'
+import { NAV } from 'config/nav.config'
 
 type AppAccountMenuProps = {
   user: User
@@ -20,7 +18,6 @@ const AppAccountMenu = ({ user }: AppAccountMenuProps) => {
   const navigate                = useNavigate()
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
   const open                    = Boolean(anchorEl)
-  const canManage               = user.role === 'admin' || user.role === 'trainer'
   const displayName             = [user.firstname, user.lastname].filter(Boolean).join(' ') || user.username || user.email
 
   const handleOpen = (event: MouseEvent<HTMLElement>) => {
@@ -37,6 +34,13 @@ const AppAccountMenu = ({ user }: AppAccountMenuProps) => {
     navigate(PATH.ROOT)
   }
 
+  const NAV_FILTERED = NAV.USER.map((item) => {
+    if (item.id === 'log-out') {
+      return { ...item, onClick: handleLogout }
+    }
+    return item
+  })
+
   return (
     <Fragment>
       <Tooltip title={transl('account')}>
@@ -48,9 +52,7 @@ const AppAccountMenu = ({ user }: AppAccountMenuProps) => {
           color={'inherit'}
           onClick={handleOpen}
         >
-          <Avatar src={user.avatar} alt={displayName} sx={{ borderRadius: 0 }}>
-            {displayName.charAt(0).toUpperCase()}
-          </Avatar>
+          <AppUserAvatar user={user} />
         </IconButton>
       </Tooltip>
       <Menu
@@ -63,38 +65,20 @@ const AppAccountMenu = ({ user }: AppAccountMenuProps) => {
         anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
       >
         <Stack spacing={0.25} sx={{ px: 2, py: 1.25, minWidth: 240 }}>
-          <Typography variant='subtitle2'>{displayName}</Typography>
-          <Typography variant='caption' color='text.secondary'>{user.email}</Typography>
-          <Typography variant='caption' color='text.secondary'>{user.role}</Typography>
+          <Typography variant={'subtitle2'}>{displayName}</Typography>
+          <Typography variant={'caption'} color={'text.secondary'}>{user.email}</Typography>
+          <Typography variant={'caption'} color={'text.secondary'}>{user.role}</Typography>
         </Stack>
         <Divider />
-        <MenuItem component={RouterLink} to={PATH.AUTH.ACCOUNT}>
-          <ListItemIcon>
-            <AccountCircleIcon fontSize='small' />
-          </ListItemIcon>
-          {transl('account')}
-        </MenuItem>
-        {canManage ? (
-          <MenuItem component={RouterLink} to={PATH.AUTH.MANAGE}>
-            <ListItemIcon>
-              <ShieldIcon fontSize={'small'} />
-            </ListItemIcon>
-           {transl('manage')}
-          </MenuItem>
-        ) : null}
-        <MenuItem component={RouterLink} to={PATH.AUTH.SETTING}>
-          <ListItemIcon>
-            <SettingsIcon fontSize={'small'} />
-          </ListItemIcon>
-          {transl('setting')}
-        </MenuItem>
-        <Divider />
-        <MenuItem onClick={handleLogout}>
-          <ListItemIcon>
-            <LogoutSharpIcon fontSize={'small'} />
-          </ListItemIcon>
-          {transl('log_out')}
-        </MenuItem>
+        {NAV_FILTERED.map((_i) => {
+          const Icon     = _i.icon
+          const isLogout = _i.id === 'log-out'
+          return (
+            <MenuItem component={!isLogout ? RouterLink : 'li'} to={_i.href} onClick={_i.onClick}>
+              <ListItemIcon>{Icon && <Icon fontSize={'small'} />}</ListItemIcon>
+              {transl(_i.label)}
+            </MenuItem>
+          )})}
       </Menu>
     </Fragment>
   )
