1a712d4e9b4485be958957ed3da4a06246c9e397
Bally.Lomibao <bally.lomibao@howickltd.com>
2026-05-31 02:35:50 +1000

    [schema]: default values for course  added

---
+++ src/lib/form/default-value/course.ts
@@ -0,0 +1,11 @@
+import { transl } from 'lib/tool'
+import { type CourseDraft } from 'types/model'
+
+export const courseDefaultValue: CourseDraft = {
+  title               : '',
+  description         : '',
+  duration            : '',
+  tuition             : 0,
+  minimumSkill        : transl('minimum_skill.beginner') as AppSkillType,
+  scholarshipAvailable: false
+}
