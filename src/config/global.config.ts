532d569cb7ec6959d9d6189df29ffe770cab201a
Bally.Lomibao <bally.lomibao@howickltd.com>
2026-06-05 01:54:14 +1000

    [fix]

--- src/config/global.config.ts
+++ src/config/global.config.ts
@@ -1,6 +1,6 @@
 export const GLOBAL = {
   APP_NAME   : import.meta.env.VITE_APP_NAME || 'TCC Projct',
-  APP_VERSION: import.meta.env.VITE_APP_VERSION || 'v1.0.0',
+  APP_VERSION: import.meta.env.VITE_APP_VERSION || 'v1',
   LOCALE     : 'en',                                            // temp impl feat: change to navbar ui changable
   PORT       : import.meta.env.PORT || 5173,
 } as const
