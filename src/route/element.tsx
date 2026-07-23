521667117f003d17537d7793318138b291a38159
Bally.Lomibao <bally.lomibao@howickltd.com>
2026-07-23 23:01:29 +1000

    [fix]: correct import path for DashboardPage component

--- src/route/element.tsx
+++ src/route/element.tsx
@@ -1,7 +1,7 @@
 import { lazy } from "react"
 import { Loadable } from "route/loadable"
 
-export const DashboardPage      = Loadable(lazy(() => import("page/dashboard/dashboard-page")))
+export const DashboardPage      = Loadable(lazy(() => import("page/dashboard/dashboard.page")))
 export const BootcampListPage   = Loadable(lazy(() => import('page/bootcamp/bootcamp-list-page')))
     export const BootcampDetailPage = Loadable(lazy(() => import('page/bootcamp/bootcamp-detail-page')))
 
