1143cc79f5e4f59517f75fda095e0449884ce5aa
Bally.Lomibao <bally.lomibao@howickltd.com>
2026-07-26 02:40:25 +1000

    [formatting

--- src/page/bootcamp/component/bootcamp-feedback-form-card.tsx
+++ src/page/bootcamp/component/bootcamp-feedback-form-card.tsx
@@ -5,38 +5,54 @@ import { transl } from 'lib/tool'
 import type { FormEvent } from 'react'
 
 interface BootcampFeedbackFormCardProps {
-    title       : string
-    setTitle    : React.Dispatch<React.SetStateAction<string>>
-    body        : string
-    setBody     : React.Dispatch<React.SetStateAction<string>>
-    rating      : number | null
-    setRating   : React.Dispatch<React.SetStateAction<number | null>>
-    createStatus: AppStateStatusType
-    canSubmit   : boolean
-    handleSubmit: (event: FormEvent<HTMLFormElement>) => Promise<void>
+  title       : string
+  setTitle    : React.Dispatch<React.SetStateAction<string>>
+  body        : string
+  setBody     : React.Dispatch<React.SetStateAction<string>>
+  rating      : number | null
+  setRating   : React.Dispatch<React.SetStateAction<number | null>>
+  createStatus: AppStateStatusType
+  canSubmit   : boolean
+  handleSubmit: (event: FormEvent<HTMLFormElement>) => Promise<void>
 }
 
-const BootcampFeedbackRatingStack = ({ rating, setRating }: { rating: number | null, setRating: React.Dispatch<React.SetStateAction<number | null>> }) => {
-    return (
-      <RatingContainerStack spacing={0.5}>
-        <Typography variant={'caption'} color={'text.secondary'}>
-          {transl('rating')}
-        </Typography>
-        <Rating
-          value={rating ? rating / 2 : null}
-          precision={0.5}
-          icon={<StarIcon fontSize={'inherit'} />}
-          emptyIcon={<StarIcon fontSize='inherit' />}
-          onChange={(_, value) => setRating(value ? value * 2 : null)}
-        />
-        <Typography variant={'caption'} color='text.secondary'>
-          {rating ? `${rating}/10` : transl('give_rating')}
-        </Typography>
-      </RatingContainerStack>
-    )
+const BootcampFeedbackRatingStack = ({
+  rating,
+  setRating,
+}: {
+  rating: number | null
+  setRating: React.Dispatch<React.SetStateAction<number | null>>
+}) => {
+  return (
+    <RatingContainerStack spacing={0.5}>
+      <Typography variant={'caption'} color={'text.secondary'}>
+        {transl('rating')}
+      </Typography>
+      <Rating
+        value={rating ? rating / 2 : null}
+        precision={0.5}
+        icon={<StarIcon fontSize={'inherit'} />}
+        emptyIcon={<StarIcon fontSize='inherit' />}
+        onChange={(_, value) => setRating(value ? value * 2 : null)}
+      />
+      <Typography variant={'caption'} color='text.secondary'>
+        {rating ? `${rating}/10` : transl('give_rating')}
+      </Typography>
+    </RatingContainerStack>
+  )
 }
 
-const BootcampFeedbackFormCard = ({ title, setTitle, body, setBody, handleSubmit, rating, setRating, canSubmit, createStatus }: BootcampFeedbackFormCardProps) => {
+const BootcampFeedbackFormCard = ({
+  title,
+  setTitle,
+  body,
+  setBody,
+  handleSubmit,
+  rating,
+  setRating,
+  canSubmit,
+  createStatus,
+}: BootcampFeedbackFormCardProps) => {
   return (
     <Card>
       <CardContent>
