fc19bdb799f71aca1c38b38aa657931cab90b61e
Bally.Lomibao <bally.lomibao@howickltd.com>
2026-07-26 19:15:50 +1000

    [refactor & enhanced]

--- src/page/dashboard/component/dashboard-learning-progress-card.tsx
+++ src/page/dashboard/component/dashboard-learning-progress-card.tsx
@@ -1,46 +1,33 @@
-import { Box, Card, CardContent, Stack, Typography, useTheme } from '@mui/material'
+import type { DashboardCourse } from 'page/dashboard/dashboard'
+import { Card, CardContent, Stack, Typography, useTheme } from '@mui/material'
 import { AssignmentIndSharp } from '@mui/icons-material'
-import { transl, type LocaleKey } from 'lib/tool'
-import type { Course } from 'types'
+import { transl } from 'lib/tool'
+import { DashboardIconBox } from 'design/styled'
 
 interface DashboardLearningProgressCardProps {
-  course  : Course
-  value   : string
-  tone    : AppThemeType
-  subtitle: LocaleKey
+  course  : DashboardCourse
 }
 
-const DashboardLearningProgressCard = ({ course, value, tone, subtitle }: DashboardLearningProgressCardProps) => {
+const DashboardLearningProgressCard = ({ course }: DashboardLearningProgressCardProps) => {
   const theme = useTheme()
-  const color = theme.palette[tone].main
+  const color = theme.palette[course.tone].main
 
   return (
     <Card sx={{ height: '100%' }}>
       <CardContent>
         <Stack spacing={1.5}>
-          <Box
-            sx={{
-              alignItems     : 'center',
-              backgroundColor: color,
-              color          : 'primary.contrastText',
-              display        : 'flex',
-              height         : 44,
-              justifyContent : 'center',
-              borderRadius   : 1,
-              width          : 44,
-            }}
-          >
+          <DashboardIconBox color={color}>
             {(<AssignmentIndSharp />)}
-          </Box>
+          </DashboardIconBox>
           <Stack>
             <Typography variant={'body2'} color={'text.secondary'}>
               {course.title}
             </Typography>
-            <Typography variant='h4' sx={{ fontWeight: 700 }}>
-              {value}
+            <Typography variant={'h4'} sx={{ fontWeight: 700 }}>
+              {course.progress}
             </Typography>
             <Typography variant={'caption'} color={'text.secondary'}>
-              {transl(subtitle)}
+              {transl(course.subtitle)}
             </Typography>
           </Stack>
         </Stack>
