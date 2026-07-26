656b77b04715cf999eba1745f6f552b532150c1a
Bally.Lomibao <bally.lomibao@howickltd.com>
2026-07-26 13:02:54 +1000

    [feat]: added new styled component

--- src/design/styled/button.tsx
+++ src/design/styled/button.tsx
@@ -1,9 +1,14 @@
+import type { ElementType } from 'react'
 import { Button } from '@mui/material'
 import { alpha, styled } from '@mui/material/styles'
-import type { ElementType } from 'react'
 
 export const SecondaryButton = styled(Button)<{ component?: ElementType, to: string }>({
     '&:hover': {
         backgroundColor: alpha('#1E242F',.5)
     }
+})
+
+export const Flex100Button = styled(Button)<{ component?: ElementType }>({
+    display: 'flex',
+    width  : '100px'
 })
